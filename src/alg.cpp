803e32349a96b6a22ed57cfeaa1bde383837b455
Peace0death <160275048+Peace0death@users.noreply.github.com>
2024-04-10 23:44:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,13 +2,95 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int sraw(char u) {
+    if (u == '+' || u == '-')
+        return 1;
+    if (u == '*' || u == '/')
+        return 2;
+    return 0;
+}
+std::string infx2post(std::string inf) {
+    // добавьте код
+    std::string pov;
+    int zero = 0;
+    TStack <char, 100> post;
+    for (char i : inf) {
+        if (i >= '0' && i <= '9') {
+            zero++;
+            if (zero == 1) {
+                pov += i;
+                continue;
+            }
+            pov = pov + ' ' + i;
+        }
+        else if (i == '+' || i == '-' || i == '*'\
+            || i == '/' || i == '(' || i == ')') {
+            if (i == '(') {
+                post.push(i);
+            }
+            else if (post.isemp()) {
+                post.push(i);
+            }
+            else if (sraw(i) > sraw(post.get())) {
+                post.push(i);
+            }
+            else if (i == ')') {
+                while (post.get() != '(') {
+                    pov = pov + ' ' + post.get();
+                    post.pop();
+                }
+                post.pop();
+            }
+            else {
+                int x = sraw(i);
+                int y = sraw(post.get());
+                while (!post.isemp() && x <= y) {
+                    pov = pov + ' ' + post.get();
+                    post.pop();
+                }
+                post.push(i);
+            }
+        }
+    }
+    while (!post.isemp()) {
+        pov = pov + ' ' + post.get();
+        post.pop();
+    }
+    return pov;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    // добавьте код
+    TStack <int, 100> number;
+    for (char i : pref) {
+        if (i >= '0' && i <= '9') {
+            number.push(i - '0');
+        }
+        else if (sraw(i)) {
+            int x = number.get();
+            number.pop();
+            int y = number.get();
+            number.pop();
+            switch (i) {
+            case '+':
+                number.push(x + y);
+                break;
+            case '-':
+                number.push(y - x);
+                break;
+            case '*':
+                number.push(x * y);
+                break;
+            case '/':
+                number.push(y / x);
+                break;
+            default:
+                continue;
+            }
+        }
+        else {
+            continue;
+        }
+    }
+    return number.get();
 }
